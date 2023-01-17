655fa1e05688061bda71183b954787464c6e2761
hrdu_ <ragnarbergseth@yahoo.no>
2023-01-17 14:41:13 +0100

    Stylishh stuff!

---
+++ script.js
@@ -0,0 +1,18 @@
+/*
+Første Teamoppgave: Inventory Filler!
+
+Dere skal ha en variabel som sparer på det dere klikker på 
+- som et "inventory" i et spill.
+Man skal kunne se/skjule hva som er i inventoryen på en måte
+
+Hint til hva som brukes for å få dette til:
+Variabel
+Parameter
+Return 
+*/
+
+// Variabler
+
+// Funksjoner
+
+function showOrHideInv() {}
