8246fdfa659cdf96c493f91517987b2051c1836e
hrdu_ <ragnarbergseth@yahoo.no>
2023-01-19 13:42:53 +0100

    Additions

--- script.js
+++ script.js
@@ -14,5 +14,27 @@ Return
 // Variabler
 
 // Funksjoner
+const tacoContainer = document.getElementById("tacoContainer");
+function selectImg(img) {
+  let newDiv = document.createElement("img");
+  newDiv.classList.add("newImg");
+  newDiv.src = img.src;
+  console.log(img.src);
+  //   let newContent= document.createElement("img")
+  tacoContainer.appendChild(newDiv);
 
+  createElement = newDiv;
+
+  console.log(createElement);
+}
 function showOrHideInv() {}
+
+/* // Create a new div element
+var newDiv = document.createElement("div");
+
+// Add some content to the div
+var newContent = document.createTextNode("Hello World!");
+newDiv.appendChild(newContent);
+
+// Add the new div to the document
+document.body.appendChild(newDiv); */
